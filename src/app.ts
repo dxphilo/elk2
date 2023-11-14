4711f68409f3bbb3e182584f62050e7575b41251
John Philip <developerphilo@gmail.com>
2023-11-15 01:25:06 +0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import { json as bodyParser } from 'body-parser';
+
+import env from './env';
+import helloWorld from './controllers/hello-world';
+import { debug } from './utilities/debug';
+
+/**
+ * This is a middleware example, and helps with debugging by outputing data for each request on the console.
+ */
+function loggingMiddleware(req: Request, res: Response, next: NextFunction) {
+    debug(`${req.method}: ${req.originalUrl}`);
+    next();
+}
+
+const app = express();
+
+// Middlewares
+app.set('port', env.port);
+app.use(loggingMiddleware);
+app.use(cors());
+app.use(bodyParser());
+
+// Controllers
+app.use(helloWorld);
+
+export default app;
